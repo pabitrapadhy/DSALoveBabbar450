d604a81654175847d549d8eb52d7185c64f24768
Pabitra Padhy <pabitrapadhycse@gmail.com>
2022-06-22 00:27:11 +0530

    fixed lb_array_13

--- arrays/lb_array_13.cpp
+++ arrays/lb_array_13.cpp
@@ -4,49 +4,53 @@
 
 using namespace std;
 
-using Intervals = vector<vector<int>>;
-
-void PrintIntervals(Intervals const& arr) {
-    for (auto const& interval : arr) {
-        cout << "{" << interval[0] << "," << interval[1] << "},";
-    }
-    cout << endl;
-}
-
-bool IsOverlapping(vector<int> const& X, vector<int> const& Y) {
-    return (Y[0] - X[1]) < 1;
-}
-
-void MergeIntervals(Intervals& arr, Intervals& ans) {
-    auto len{arr.size()};
-    if (len == 1) {
-        ans = arr;
-        return;
-    }
-
-    sort(arr.begin(), arr.end()); // sorted
-
-    auto maxInterval{arr[0]}; // set 1st item to max interval
-
-    for (auto it = next(arr.begin()); it != arr.end(); ++it) {
-        auto interval{*it};
-        if (IsOverlapping(maxInterval, interval)) {
-            maxInterval[1] = max(maxInterval[1], interval[1]); // update range
-        } else {
-            ans.push_back(maxInterval);
-            maxInterval = interval;
+bool HasAllNegativeElements(vector<int> const& arr, int len) {
+    for (int i = 0; i < len; ++i) {
+        if (arr[i] > 0) {
+            return false;
         }
     }
+    return true;
+}
 
-    ans.push_back(maxInterval);
+int FindMaxElement(vector<int> const& arr, int len) {
+    int maximum{arr[0]};
+    for (int i = 1; i < len; ++i) {
+        maximum = max(arr[i], maximum);
+    }
+    return maximum;
+}
+
+long long MaximumSumSubarray(vector<int> arr) {
+    int len{static_cast<int>(arr.size())};
+
+    if (HasAllNegativeElements(arr, len)) {
+        return FindMaxElement(arr, len);
+    } else {
+        int start{0}, last{0};
+        long long mss{arr[0]};
+        long long sum{arr[0]};
+
+        for (int i = 1; i < len; ++i) {
+            if (sum < 0) { // check for sum reset
+                sum = 0;
+                start = i;
+            }
+
+            sum += arr[i]; // add element to sum
+
+            if (sum > mss) { // check for max
+                mss = sum;
+                last = i;
+            }
+        }
+        return mss;
+    }
 }
 
 int main() {
-    // [[1,3],[2,6],[8,10],[15,18]]
-    // Intervals arr{{1,3},{2,6},{8,10},{15,18}};
-    Intervals arr{{1,4},{2,3}};
-    Intervals ans{};
-    PrintIntervals(arr);
-    MergeIntervals(arr, ans); 
-    PrintIntervals(ans);
+    // vector<int> arr{-1,-2,-3,-4};
+    vector<int> arr{1,2,3,-2,5};
+
+    cout << MaximumSumSubarray(arr);
 }
